55a802bcee990757c5092a22ca66711bee1a8e56
Mieke Nijs <mieke.nijs@smals.be>
2024-12-10 13:38:39 +0000

    Bug/uhmep 1700

---
+++ reuse/code/pipes/can-reject-assignation.pipe.spec.ts
@@ -0,0 +1,114 @@
+import { CanRejectAssignationPipe } from './can-reject-assignation.pipe';
+import { AccessMatrixState } from '../states/access-matrix.state';
+import { PerformerTask, ReadPrescription, Status, TaskStatus } from '../interfaces';
+
+const patientSsin = '123';
+const currentUserSsin = '456';
+const caregiverSsin = '789';
+describe('CanRejectAssignationPipe', () => {
+  let pipe: CanRejectAssignationPipe;
+  let mockAccessMatrixState: jest.Mocked<AccessMatrixState>;
+
+  beforeEach(() => {
+    mockAccessMatrixState = {
+      hasAtLeastOnePermission: jest.fn(),
+    } as unknown as jest.Mocked<AccessMatrixState>;
+
+    pipe = new CanRejectAssignationPipe(mockAccessMatrixState);
+  });
+
+  it('should return false if currentUserSsin is not provided', () => {
+    const prescription = {} as ReadPrescription;
+    const task = {} as PerformerTask;
+    const result = pipe.transform(prescription, task, patientSsin);
+    expect(result).toBe(false);
+  });
+
+  it('should return false if the prescription status is not OPEN or IN_PROGRESS', () => {
+    mockAccessMatrixState.hasAtLeastOnePermission.mockReturnValue(true);
+    const prescription = { status: Status.BLACKLISTED } as ReadPrescription;
+    const task = { status: TaskStatus.READY, careGiver: { ssin: caregiverSsin } } as PerformerTask;
+
+    const result = pipe.transform(prescription, task, patientSsin, currentUserSsin);
+    expect(result).toBe(false);
+  });
+
+  it('should return false if the task status is not READY', () => {
+    mockAccessMatrixState.hasAtLeastOnePermission.mockReturnValue(true);
+    const prescription = { status: Status.OPEN } as ReadPrescription;
+    const task = { status: TaskStatus.COMPLETED, careGiver: { ssin: caregiverSsin } } as PerformerTask;
+
+    const result = pipe.transform(prescription, task, patientSsin, currentUserSsin);
+    expect(result).toBe(false);
+  });
+
+  it('should return false if currentUserSsin is not the patient nor the caregiver', () => {
+    mockAccessMatrixState.hasAtLeastOnePermission.mockReturnValue(true);
+    const prescription = { status: Status.OPEN } as ReadPrescription;
+    const task = { status: TaskStatus.READY, careGiver: { ssin: caregiverSsin } } as PerformerTask;
+
+    const result = pipe.transform(prescription, task, patientSsin, 'anotherSsin');
+    expect(result).toBe(false);
+  });
+
+  it('should call hasAssignPermissions with the correct arguments for "proposal" intent', () => {
+    const prescription = {
+      intent: 'proposal',
+      templateCode: 'template1',
+      status: Status.OPEN,
+    } as ReadPrescription;
+
+    const task = { status: TaskStatus.READY, careGiver: { ssin: currentUserSsin } } as PerformerTask;
+    mockAccessMatrixState.hasAtLeastOnePermission.mockReturnValue(true);
+
+    const result = pipe.transform(prescription, task, patientSsin, currentUserSsin);
+    expect(mockAccessMatrixState.hasAtLeastOnePermission).toHaveBeenCalledWith(['removeAssignationProposal'], 'template1');
+    expect(result).toBe(true);
+  });
+
+  it('should call hasAssignPermissions with the correct arguments for "prescription" intent', () => {
+    const prescription = {
+      intent: 'prescription',
+      templateCode: 'template2',
+      status: Status.IN_PROGRESS,
+    } as ReadPrescription;
+
+    const task = { status: TaskStatus.READY, careGiver: { ssin: currentUserSsin } } as PerformerTask;
+    mockAccessMatrixState.hasAtLeastOnePermission.mockReturnValue(true);
+
+    const result = pipe.transform(prescription, task, patientSsin, currentUserSsin);
+    expect(mockAccessMatrixState.hasAtLeastOnePermission).toHaveBeenCalledWith(['removeAssignationPrescription'], 'template2');
+    expect(result).toBe(true);
+  });
+
+  it('should return true if all conditions are met', () => {
+    const prescription = {
+      intent: 'prescription',
+      templateCode: 'template2',
+      status: Status.OPEN,
+    } as ReadPrescription;
+
+    const task = { status: TaskStatus.READY, careGiver: { ssin: currentUserSsin } } as PerformerTask;
+
+    mockAccessMatrixState.hasAtLeastOnePermission.mockReturnValue(true);
+
+    const result = pipe.transform(prescription, task, patientSsin, currentUserSsin);
+    expect(result).toBe(true);
+  });
+
+  it('should return false if permissions are not granted', () => {
+    const prescription = {
+      intent: 'proposal',
+      templateCode: 'template3',
+      status: Status.OPEN,
+    } as ReadPrescription;
+
+    const task = { status: TaskStatus.READY, careGiver: { ssin: currentUserSsin } } as PerformerTask;
+
+    mockAccessMatrixState.hasAtLeastOnePermission.mockReturnValue(false);
+
+    const result = pipe.transform(prescription, task, patientSsin, currentUserSsin);
+    expect(result).toBe(false);
+  });
+
+});
--- reuse/code/pipes/can-reject-assignation.pipe.ts
+++ reuse/code/pipes/can-reject-assignation.pipe.ts
@@ -2,18 +2,46 @@ import { Pipe } from '@angular/core';
 import { PerformerTask, ReadPrescription, Status, TaskStatus } from '../interfaces';
 import { AccessMatrixState } from '../states/access-matrix.state';
 
+/**
+ * This pipe determines whether an assignation can be rejected.
+ *
+ * The access matrix needs to have removeAssignationPrescription or removeAssignationProposal depending on the intent
+ * The status of the prescription can be OPEN or IN_PROGRESS
+ * The status of the task needs to be READY
+ * The caregiver assigned to the task and the patient assigned to the prescription can reject an assignation
+ *
+ * Example usage:
+ * ```html
+ * <button *ngIf="assignation | canRejectAssignation : performertask : patientSSIN : currentUserSSIN">Reject</button>
+ * ```
+ *
+ * @pipe
+ * @name CanRejectAssignationPipe
+ */
 @Pipe({name: 'canRejectAssignation', standalone: true})
 export class CanRejectAssignationPipe {
 
   constructor(private accessMatrixState: AccessMatrixState) {
   }
 
-  transform(prescription: ReadPrescription, task: PerformerTask, currentUserSsin?: string): boolean {
-    if (currentUserSsin == undefined)
+  transform(prescription: ReadPrescription, task: PerformerTask, patientSsin: string, currentUserSsin?: string): boolean {
+    if (!currentUserSsin)
       return false;
-    return this.accessMatrixState.hasAtLeastOnePermission(['executeTreatment'], prescription.templateCode)
+    return this.hasAssignPermissions(prescription)
       && prescription.status != null && [Status.OPEN, Status.IN_PROGRESS].includes(prescription.status)
       && task?.status === TaskStatus.READY
-      && currentUserSsin === task.careGiver.ssin;
+      && this.checkIfCurrentUserIsPatientOrAssignedCaregiver(currentUserSsin, patientSsin, task.careGiver.ssin);
+  }
+
+  private checkIfCurrentUserIsPatientOrAssignedCaregiver(currentUserSsin:string, patientSsin:string, caregiverSsin: string) {
+    return currentUserSsin === caregiverSsin || currentUserSsin === patientSsin
+  }
+
+  private hasAssignPermissions(prescription: ReadPrescription) {
+    const intent = prescription.intent
+    if(intent === 'proposal') {
+      return this.accessMatrixState.hasAtLeastOnePermission(['removeAssignationProposal'], prescription.templateCode);
+    }
+    return this.accessMatrixState.hasAtLeastOnePermission(['removeAssignationPrescription'], prescription.templateCode);
   }
 }
