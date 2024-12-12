d3ac1080faf265d6218526d4983bd06a18bb2ab7
Mieke Nijs <mieke.nijs@smals.be>
2024-12-12 14:37:22 +0000

    bug/UHMEP-1714

--- reuse/code/pipes/can-extend-prescription.pipe.ts
+++ reuse/code/pipes/can-extend-prescription.pipe.ts
@@ -2,6 +2,21 @@ import { Pipe } from '@angular/core';
 import { AccessMatrixState } from '../states/access-matrix.state';
 import {ReadPrescription, Status} from "../interfaces";
 
+/**
+ * This pipe determines whether a prescription can be extended.
+ *
+ * The access matrix needs to have createPrescription
+ * The status of the prescription can be OPEN or IN_PROGRESS
+ * The end date needs to be in the future
+ *
+ * Example usage:
+ * ```html
+ * <button *ngIf="prescription | canExtendPrescription">Extend</button>
+ * ```
+ *
+ * @pipe
+ * @name canExtendPrescription
+ */
 @Pipe({name: 'canExtendPrescription', standalone: true})
 export class CanExtendPrescriptionPipe {
 
@@ -10,9 +25,14 @@ export class CanExtendPrescriptionPipe {
   ) {
   }
 
-  transform(templateCode: string, prescription: ReadPrescription): boolean {
-    return this.accessMatrixState.hasAtLeastOnePermission(['createPrescription'], templateCode)
+  transform(prescription: ReadPrescription): boolean {
+    const now = new Date();
+    const endDate = prescription.period?.end ? new Date(prescription.period.end) : false
+
+    return this.accessMatrixState.hasAtLeastOnePermission(['createPrescription'], prescription.templateCode)
       && !!prescription.status
-      && [Status.OPEN, Status.ON_HOLD, Status.IN_PROGRESS, Status.CANCELLED].includes(prescription.status);
+      && [Status.OPEN, Status.IN_PROGRESS].includes(prescription.status)
+      && endDate
+      && endDate > now;
   }
 }
