e7b62c2d48307c03b658b45e813f1f5f20992892
mini <mieke.nijs@smals.be>
2025-03-24 15:49:56 +0100

    add toggle tests

--- reuse/code/components/toggle-historic-prescriptions/toggle-historic-prescriptions.component.spec.ts
+++ reuse/code/components/toggle-historic-prescriptions/toggle-historic-prescriptions.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToggleHistoricPrescriptionsComponent } from './toggle-historic-prescriptions.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { By } from '@angular/platform-browser';
 
-describe('FilterPrescriptionsComponent', () => {
+describe('ToggleHistoricPrescriptionsComponent', () => {
   let component: ToggleHistoricPrescriptionsComponent;
   let fixture: ComponentFixture<ToggleHistoricPrescriptionsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToggleHistoricPrescriptionsComponent]
+      imports: [ToggleHistoricPrescriptionsComponent, TranslateModule.forRoot()],
+      providers: [
+        TranslateService
+      ]
     })
       .compileComponents();
 
@@ -19,5 +24,24 @@ describe('FilterPrescriptionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize slide toggle with isChecked value', () => {
+    component.isChecked = true;
+    fixture.detectChanges();
+
+    const toggle = fixture.debugElement.query(By.css('mat-slide-toggle'))
+      .nativeElement as HTMLInputElement;
+
+    expect(toggle.getAttribute('ng-reflect-model')).toBe('true');
+  });
+
+  it('should emit toggleChanged event when the toggle is clicked', () => {
+    jest.spyOn(component.toggleChanged, 'emit');
+
+    const toggle = fixture.debugElement.query(By.css('mat-slide-toggle'));
+    toggle.triggerEventHandler('change', {checked: true});
+
+    expect(component.toggleChanged.emit).toHaveBeenCalledWith(true);
   });
 });
