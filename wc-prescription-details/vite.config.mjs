65b4a88ee0fb142dec77d383fd9edfb6c4e418fe
Mieke Nijs <mieke.nijs@smals.be>
2025-10-20 12:08:39 +0000

    Feature/uhmep 2223

--- wc-prescription-details/vite.config.mjs
+++ wc-prescription-details/vite.config.mjs
@@ -1,7 +1,6 @@
-import {defineConfig} from "vite";
-import {viteStaticCopy} from 'vite-plugin-static-copy'
-import path from 'path'
-
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -11,36 +10,32 @@ export default defineConfig({
           src: './../dist/evf-form-details/build/evf-form-details.js',
           dest: './assets/evf-form-details',
         },
-        {
-          src: './../dist/pdfmake/build/pdfmake.js',
-          dest: './assets/pdfmake',
-        },
       ],
-    }),],
-  root: "wc-prescription-details",
+    }),
+  ],
+  root: 'wc-prescription-details',
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: "[name].js",
+        entryFileNames: '[name].js',
         assetFileNames: `[name].[ext]`,
         manualChunks(id) {
-
-          if(id.includes('styles')) {
-            return 'wc-prescription-details'
+          if (id.includes('styles')) {
+            return 'wc-prescription-details';
           }
 
-          const idArr = id.split("/");
+          const idArr = id.split('/');
           const lastItem = idArr.pop();
-          const stringArr = lastItem.split(".");
+          const stringArr = lastItem.split('.');
 
           return stringArr[0];
         },
       },
       input: {
-        "wc-prescription-details": path.resolve(__dirname, 'build.js'),
-      }
+        'wc-prescription-details': path.resolve(__dirname, 'build.js'),
+      },
     },
     outDir: './../dist/wc-prescription-details/build',
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 });
