fc2577396b33e6d608053e0241f829a0f9dd9a60
jubl <jules.blothiaux@smals.be>
2025-12-08 15:52:46 +0100

    [UHMEP-2563] fix patient ssin randomly disappears of details by simplifying ssinPipe

--- reuse/code/pipes/format-ssin.pipe.ts
+++ reuse/code/pipes/format-ssin.pipe.ts
@@ -2,24 +2,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'formatSsin', standalone: true })
 export class FormatSsinPipe implements PipeTransform {
-  private static readonly mask = '00.00.00-000.00';
-  private static readonly maskParts = FormatSsinPipe.mask.split(/[^0-9]/);
-  private static readonly maskSeparators = FormatSsinPipe.mask.replace(/[0-9]/g, '').split('');
 
   transform(value?: string): string {
     if (!value) {
       return '';
     }
-    value = value.replace(/[^0-9]+/, '');
-    const formattedParts = [];
-    for (const part of FormatSsinPipe.maskParts) {
-      const endIndex = value.length >= part.length ? part.length : value.length;
-      formattedParts.push(value.substring(0, endIndex));
-      value = value.substring(endIndex);
+    const digits = value.replace(/\D+/g, '');
+
+    if (digits.length !== 11) {
+      return digits.toString();
     }
-    return formattedParts.reduce(
-      (acc, cur, index) => acc + cur + (index < formattedParts.length - 1 ? FormatSsinPipe.maskSeparators[index] : ''),
-      ''
-    );
+
+    const year = digits.substring(0, 2);
+    const month = digits.substring(2, 4);
+    const day = digits.substring(4, 6);
+    const order = digits.substring(6, 9);
+    const control = digits.substring(9, 11);
+
+    return `${year}.${month}.${day}-${order}.${control}`;
   }
 }
